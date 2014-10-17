5c5e03bc1bbf81de1ff9b686c5baeb16ceebed08
Andy Dorman <andrew.dorman@piksel.com>
2014-10-17 22:00:27 +0100

    updated db connection string

--- lib/db.php
+++ lib/db.php
@@ -2,7 +2,7 @@
 ini_set('display_errors',1); 
 error_reporting(E_ALL);
 //$mysqli = new mysqli("127.0.0.1", "root", "", "gazelli", 3306);
-$mysqli = mysql_connect("localhost", "walton", "w4lt0ns3cret");
+$mysqli = mysql_connect("localhost", "walton", "w4lt0ns3cr3t");
 if (!$mysqli) {
     die('Could not connect: ' . mysql_error());
 }
