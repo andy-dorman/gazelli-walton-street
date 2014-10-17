a9df898505117446674ae0a022e628dc61cad3d2
Andy Dorman <andrew.dorman@piksel.com>
2014-10-17 21:50:36 +0100

    updated db connection string

--- lib/db.php
+++ lib/db.php
@@ -2,9 +2,9 @@
 ini_set('display_errors',1); 
 error_reporting(E_ALL);
 //$mysqli = new mysqli("127.0.0.1", "root", "", "gazelli", 3306);
-$mysqli = mysql_connect("localhost", "walton-street", "w4lt0n");
+$mysqli = mysql_connect("localhost", "walton", "w4lt0n");
 if (!$mysqli) {
     die('Could not connect: ' . mysql_error());
 }
-mysql_select_db("walton-street", $mysqli);
+mysql_select_db("waltonsecret", $mysqli);
 ?>
