3bebd4cd5d5ea7a5c2c5ee385f03ed58edfd917d
Andy Dorman <andrew.dorman@piksel.com>
2014-10-18 11:17:56 +0100

    click focuses goal items for mobiles

--- index.php
+++ index.php
@@ -50,7 +50,18 @@ function getInitals($name) {
   <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
   <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 <![endif]-->
-
+<?php
+if($detect->isMobile() || $detect->isTablet()) {
+?>
+$(document.ready)(function(){
+  $('key-panel.goal').click(function(){
+    $('key-panel.goal').blur();
+    this.focus();
+  });
+})
+<?php
+}
+?>
 <!-- Favicons -->
 <link rel="apple-touch-icon-precomposed" href="/apple-touch-icon-precomposed.png">
 <link rel="icon" href="/favicon.ico">
