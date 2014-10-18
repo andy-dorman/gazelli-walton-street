77723375669c1615d1315878c4965ce6646093a3
Andy Dorman <andrew.dorman@piksel.com>
2014-10-18 11:40:41 +0100

    click focuses goal items for mobiles

--- index.php
+++ index.php
@@ -50,25 +50,7 @@ function getInitals($name) {
   <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
   <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 <![endif]-->
-<?php
-if($detect->isMobile() || $detect->isTablet()) {
-?>
-<script type="text/javascript">
 
-$(document.ready)(function(){
-  alert($('.key-panel.goal').size());
-  $('.key-panel.goal').each(function(index, value) {
-    $(this).on("touchstart", function(){
-      alert("hover");
-      $('.key-panel.goal').removeClass("hover");
-      $(this).addClass("hover");
-    });
-  });
-})
-</script>
-<?php
-}
-?>
 <!-- Favicons -->
 <link rel="apple-touch-icon-precomposed" href="/apple-touch-icon-precomposed.png">
 <link rel="icon" href="/favicon.ico">
@@ -226,7 +208,25 @@ $(document.ready)(function(){
 <!-- Placed at the end of the document so the pages load faster -->
 <script src="javascripts/walton-street.min.js"></script>
 
+<?php
+if($detect->isMobile() || $detect->isTablet()) {
+?>
+<script type="text/javascript">
 
+$(document.ready)(function(){
+  alert($('.key-panel.goal').size());
+  $('.key-panel.goal').each(function(index, value) {
+    $(this).on("touchstart", function(){
+      alert("hover");
+      $('.key-panel.goal').removeClass("hover");
+      $(this).addClass("hover");
+    });
+  });
+})
+</script>
+<?php
+}
+?>
 
 <script>
   window.twttr = (function (d,s,id) {
