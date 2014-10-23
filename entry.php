c742eb1c51f302ea1a6d7f00e5ff6ea1ef122abb
Andy Dorman <andrew.dorman@piksel.com>
2014-10-23 20:17:15 +0100

    tweaked email for outlook.com

--- entry.php
+++ entry.php
@@ -58,16 +58,7 @@ function sendEmail($email, $code, $goal, $name, $id) {
 	$message .="<table cellpadding='0' cellspacing='0' border='0'>\n";
 	$message .="<tr>\n";
 	$message .="<td width='250' height='65'><img style='display: block; min-width: 250px; width:250px; height: 65px;' width='250' height='65' src='http://www.waltonsecret.com/images/email/top-row-3-left.png'/></td>\n";
-    $message .= "<td width='305' height='65'>\n";
-	$message .="<table cellpadding='0' cellspacing='0' border='0' width='305'>\n";
-    $message .="<tr>\n";
-    $message .="<td height='10' style='height:10px'></td>\n";
-    $message .="</tr>\n";
-    $message .="<tr>\n";
-    $message .="<td align='center' height='55' width='305' style='margin: 0; width: 305px; height: 55px; line-height: 1.2em; color: #AAAAAA; font-size: 12px; font-family: \"Century Gothic\", CenturyGothic, AppleGothic, sans-serif; margin: 0;'>Keep your key safe to receive your special gift after out launch in 2015. You will be contacted with further details on claiming you gift at a later date.</td>\n";
-    $message .="</tr>\n";
-    $message .="</table>\n";
-    $message .="</td>\n";
+    $message .="<td align='center' height='55' width='305' style='margin: 0; width: 305px; height: 55px; line-height: 1.2em; color: #AAAAAA; font-size: 12px; font-family: \"Century Gothic\", CenturyGothic, AppleGothic, sans-serif; margin: 0;'><p>Keep your key safe to receive your special gift after out launch in 2015. You will be contacted with further details on claiming you gift at a later date.</p></td>\n";
 	$message .="<td width='245' height='65'><img style='display: block; min-width: 245px; width:245px; height: 65px;' width='245' height='65' src='http://www.waltonsecret.com/images/email/top-row-3-right.png'/></td>\n";
 	$message .="</tr>\n";
 	$message .="</table>\n";
@@ -174,7 +165,7 @@ function sendEmail($email, $code, $goal, $name, $id) {
 	$message .="<td  style='height: 10px;' height='10'>\n";
 	$message .="</tr>\n";
 	$message .="<tr>\n";
-	$message .="<td width='260' style='vertical-align: middle; line-height: 1.2em; font-size: 12px; margin: 0; padding: 0;font-family: \"Century Gothic\", CenturyGothic, AppleGothic, sans-serif; color: #AAAAAA; text-align:center'>Share the Walton Secret with your friends and help them find their key</td>\n";
+	$message .="<td width='260' style='vertical-align: middle; line-height: 1.2em; font-size: 12px; margin: 0; padding: 0;font-family: \"Century Gothic\", CenturyGothic, AppleGothic, sans-serif; color: #AAAAAA; text-align:center'><p>Share the Walton Secret with your friends and help them find their key</p></td>\n";
 	$message .="</tr>\n";
 	$message .="<tr>\n";
 	$message .="<td style='height: 10px;' height='10'>\n";
